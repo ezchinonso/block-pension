7cf020d9b54da67147f16426e0709511c9e590e9
Nonso <ezechinonsof@gmail.com>
2020-12-17 11:54:14 +0100

    update frontend ropsten testnet address

--- client/src/App.js
+++ client/src/App.js
@@ -154,7 +154,7 @@ const App =()=>{
         const web3Provider = new ethers.providers.Web3Provider(ethereum);
         const signer = web3Provider.getSigner(account);
         const pensionContract = new ethers.Contract(
-          '0xd3D4Ad85B1d7a46c5834182CD50947c5FAD21948',
+          '0x821410748dD37B12b59d2Dc476bB9A435D5DC7Fd',
           PensionScheme.abi,
           signer
           );      
